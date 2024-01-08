95b134b72af4298d74943b42f2ed566b3ec2cf86
Dorian <dorianoriginaldesings@gmail.com>
2024-01-08 13:59:47 +0100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'standard',
+		'prettier'
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {
+		'react/prop-types': 'off'
+	}
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <h1>Nucleo de la aplicación</h1>;
+};
+
+export default App;
